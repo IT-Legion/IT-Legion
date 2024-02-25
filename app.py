83f29db4a9f90a5523ec3c56825d7826c7188cbd
Сергей Перевозников <parik073@gmail.com>
2024-02-25 21:06:38 +0200

    Initial

---
+++ app.py
@@ -0,0 +1,38 @@
+# Импорт класса Flask из библиотеки Flask и функции render_template для рендеринга HTML-шаблонов
+from flask import Flask, render_template  
+
+
+# Создание экземпляра веб-приложения Flask с указанием текущего модуля в качестве имени
+app = Flask(__name__)
+
+
+
+# Декоратор для установки маршрута '/' для вызова функции index
+@app.route('/home')
+@app.route('/')
+def index():  
+    # Функция для отображения главной страницы приложения
+    return render_template('main/index.html')   
+
+# Декоратор для установки маршрута '/about' для вызова функции about
+@app.route('/about')
+def about():  
+    # Функция для отображения страницы о нас 
+    return render_template('main/about.html')   
+
+# Декоратор для установки маршрута '/about' для вызова функции about
+@app.route('/partners')
+def partners():  
+    # Функция для отображения страницы о нас 
+    return render_template('main/partners.html') 
+
+
+
+
+
+
+
+# Условие для проверки, запущен ли этот скрипт напрямую
+if __name__ == '__main__':  
+    # Запуск веб-приложения Flask в режиме отладки
+    app.run(debug=True)  
